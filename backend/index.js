2749c7ed165e627589197c13d309b5cc12b64b94
Adham Sherbiny <98004101+Adhamsherbiny@users.noreply.github.com>
2024-06-26 22:08:52 +0300

    Check some Erros

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import userRoute from "./routes/userroute.js"
-
+import databaseConnect from "./database/database.js";
+import { adminRouter } from "./routes/adminroutes.js";
 const port = 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use("/user" , userRoute)
+app.use("/admins" , adminRouter)
 
 app.listen(port , () => {console.log(`App is listenning port ${port} and server is running`)})
