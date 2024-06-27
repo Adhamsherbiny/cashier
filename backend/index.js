adcefdaceb4218e443429d44503f0e41f660b8b1
Adham Sherbiny <98004101+Adhamsherbiny@users.noreply.github.com>
2024-06-27 13:14:51 +0300

    new edit to upload backend

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,17 @@ import cors from "cors";
 import { adminRouter } from "./routes/adminroutes.js";
 const port = 5000
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin:["https://cashier-npyhywfjw-adhamsherbinys-projects.vercel.app/"],
+    methods: ["POST" , "GET"],
+    Credential:true
+}
+))
 app.use(express.json())
 app.use("/admins" , adminRouter)
+
+app.get("/" , (req , res)=>{
+    res.send("Api is running")
+})
 
 app.listen(port , () => {console.log(`App is listenning port ${port} and server is running`)})
