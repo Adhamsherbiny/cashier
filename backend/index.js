30a5065eb094843fa949ba2bf90bc5ab3f64a51d
Adham Sherbiny <98004101+Adhamsherbiny@users.noreply.github.com>
2024-06-27 13:57:54 +0300

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { adminRouter } from "./routes/adminroutes.js";
 const port = 5000
 const app = express()
 app.use(express.json())
@@ -15,5 +14,4 @@ app.get("/" , (req , res)=>{
 })
 
 
-app.use("/admins" , adminRouter)
 app.listen(port , () => {console.log(`App is listenning port ${port} and server is running`)})
