803936617ab59a37f6ea4a6bbfce96e98c7e97af
Adham Sherbiny <98004101+Adhamsherbiny@users.noreply.github.com>
2024-06-27 13:55:54 +0300

    updated some futures

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,17 @@ import cors from "cors";
 import { adminRouter } from "./routes/adminroutes.js";
 const port = 5000
 const app = express()
+app.use(express.json())
 app.use(cors({
     origin:"*",
     methods: ["POST" , "GET"],
     Credential:true
 }
 ))
-app.use(express.json())
-app.use("/admins" , adminRouter)
-
 app.get("/" , (req , res)=>{
     res.send("Api is running")
 })
 
+
+app.use("/admins" , adminRouter)
 app.listen(port , () => {console.log(`App is listenning port ${port} and server is running`)})
